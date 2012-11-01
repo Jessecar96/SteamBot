1a2796818f99063494ca80b2bfffa20790d91a4c
cwhelchel <c.whelchel@gmail.com>
2012-11-01 11:24:51 -0400

    Update AssemblyInfo to match others.

--- SteamTrade/AssemblyInfo.cs
+++ SteamTrade/AssemblyInfo.cs
@@ -8,8 +8,8 @@ using System.Runtime.CompilerServices;
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("")]
-[assembly: AssemblyCopyright("cwhelchel")]
+[assembly: AssemblyProduct("SteamTrade")]
+[assembly: AssemblyCopyright("C) 2012 SteamBot Contributors")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -17,7 +17,7 @@ using System.Runtime.CompilerServices;
 // The form "{Major}.{Minor}.*" will automatically update the build and revision,
 // and "{Major}.{Minor}.{Build}.*" will update just the revision.
 
-[assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("0.0.1.*")]
 
 // The following attributes are used to specify the signing key for the assembly, 
 // if desired. See the Mono documentation for more information about signing.
