847d8e557da2c9c350cb285354b03354862f5de1
Lachlan <PaymhGaming@gmail.com>
2013-01-04 16:35:35 +1000

    Command line switch to set settings file

--- SteamBot/Program.cs
+++ SteamBot/Program.cs
@@ -2,6 +2,7 @@ using System;
 using System.Threading;
 using SteamKit2;
 using SteamTrade;
+using System.IO;
 
 namespace SteamBot
 {
@@ -9,12 +10,45 @@ namespace SteamBot
     {
         public static void Main(string[] args)
         {
-            if (System.IO.File.Exists("settings.json"))
+            if (args.Length > 0)
             {
-                Configuration config = Configuration.LoadConfiguration("settings.json");
+                for (int i = 0; i < args.Length; i++)
+                {
+                    if (args[i] == "-help")
+                    {
+                        Console.ForegroundColor = ConsoleColor.Green;
+                        Console.WriteLine("Type -s [file name], to load from a different config file");
+                        Console.ForegroundColor = ConsoleColor.White;
+                        return;
+                    }
+                    if (args[i] == "-s")
+                    {
+                        string SettingsFile = args[i + 1].Replace("\"", "");
+                        if (!SettingsFile.Contains(".json"))
+                        {
+                            SettingsFile = SettingsFile + ".json";
+                        }
+                        Load(SettingsFile);
+                        return;
+                    }
+                }
+            }
+            else
+            {
+                Load();
+                return;
+            }
+        }
+
+        private static void Load(string file = "settings.json")
+        {
+            if (System.IO.File.Exists(file))
+            {
+                Configuration config = Configuration.LoadConfiguration(file);
                 Log mainLog = new Log(config.MainLog, null);
                 foreach (Configuration.BotInfo info in config.Bots)
                 {
+                    mainLog.Info("Loading Settings From File " + file + ".");
                     mainLog.Info("Launching Bot " + info.DisplayName + "...");
                     new Thread(() =>
                     {
@@ -23,9 +57,10 @@ namespace SteamBot
                         {
                             try
                             {
-                                new Bot(info, config.ApiKey, (Bot bot, SteamID sid) => {
-                                    
-                                    return (SteamBot.UserHandler)System.Activator.CreateInstance(Type.GetType(bot.BotControlClass), new object[] { bot, sid });  
+                                new Bot(info, config.ApiKey, (Bot bot, SteamID sid) =>
+                                {
+
+                                    return (SteamBot.UserHandler)System.Activator.CreateInstance(Type.GetType(bot.BotControlClass), new object[] { bot, sid });
                                 }, false);
 
                             }
@@ -41,8 +76,16 @@ namespace SteamBot
             }
             else
             {
-                Console.WriteLine("Configuration File Does not exist. Please rename 'settings-template.json' to 'settings.json' and modify the settings to match your environment");
+                if (file == "settings.json")
+                {
+                    Console.WriteLine("Configuration File Does not exist. Please rename 'settings-template.json' to 'settings.json' and modify the settings to match your environment");
+                }
+                else
+                {
+                    Console.WriteLine("Configuration File Does not exist. Please ensure the file specified exists and modify the settings to match your environment");
+                }
             }
+
         }
     }
 }
