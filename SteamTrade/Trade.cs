Jessecar96/SteamBot

commit ce285d96e9a9f9b00b685ad925e0fe7347829757

Change a Trade Message from the Bot.
- This also lays the ground work for a remove command
- ground work for an "all" items sub command

SteamTrade/Trade.cs
@@ -380,6 +380,34 @@ namespace SteamTrade
         }
 
         /// <summary>
+        /// Removes an entire set of items by Defindex.
+        /// </summary>
+        /// <param name="defindex">The defindex. (ex. 5022 = crates)</param>
+        /// <param name="numToRemove">The upper limit on amount of items to remove. <c>0</c> to remove all items.</param>
+        /// <returns>Number of items removed.</returns>
+        public uint RemoveAllItemsByDefindex (int defindex, uint numToRemove = 0)
+        {
+            List<Inventory.Item> items = myInventory.GetItemsByDefindex(defindex);
+
+            uint removed = 0;
+
+            foreach (Inventory.Item item in items)
+            {
+                if (item != null && !myOfferedItems.ContainsValue(item.Id))
+                {
+                    bool success = RemoveItem (item.Id);
+
+                    if (success) removed++;
+
+                    if (numToRemove > 0 && removed >= numToRemove)
+                        return removed;
+                }
+            }
+
+            return removed;
+        }
+
+        /// <summary>
         /// Sends a message to the user over the trade chat.
         /// </summary>
         public bool SendMessage (string msg)

SteamBot/AdminUserHandler.cs
@@ -11,9 +11,11 @@ namespace SteamBot
     public class AdminUserHandler : UserHandler
     {
         private const string AddCmd = "add";
+        private const string RemoveCmd = "remove";
         private const string AddCratesSubCmd = "crates";
         private const string AddWepsSubCmd = "weapons";
         private const string AddMetalSubCmd = "metal";
+        private const string AllSubCmd = "all";
         private const string HelpCmd = "help";
 
         public AdminUserHandler(Bot bot, SteamID sid)
@@ -80,7 +82,7 @@ namespace SteamBot
 
         public override void OnTradeInit()
         {
-            Trade.SendMessage("Tell me what to do, Master. (Type " + HelpCmd + " for commands.)");
+            Trade.SendMessage("Success. (Type " + HelpCmd + " for commands.)");
         }
 
         public override void OnTradeAddItem(Schema.Item schemaItem, Inventory.Item inventoryItem)
@@ -139,6 +141,8 @@ namespace SteamBot
 
             if (message.StartsWith(AddCmd))
                 HandleAddCommand(message);
+            else if (message.StartsWith(RemoveCmd))
+                HandleRemoveCommand(message);
         }
 
         private void PrintHelpMessage()
@@ -174,13 +178,13 @@ namespace SteamBot
 
             switch (typeToAdd)
             {
-                case "metal":
+                case AddMetalSubCmd:
                     AddItemsByCraftType("craft_bar", amount);
                     break;
-                case "weapons":
+                case AddWepsSubCmd:
                     AddItemsByCraftType("weapon", amount);
                     break;
-                case "crates":
+                case AddCratesSubCmd:
                     AddItemsByCraftType("supply_crate", amount);
                     break;
                 default:
@@ -188,6 +192,21 @@ namespace SteamBot
                     break;
             }
         }
+
+
+
+        private void HandleRemoveCommand(string command)
+        {
+            var data = command.Split(' ');
+
+            string subCommand;
+
+            bool subCmdOk = GetSubCommand(data, out subCommand);
+
+            if (!subCmdOk)
+                return;
+        }
+
 
         private void AddItemsByCraftType(string typeToAdd, uint amount)
         {

commit 0bb85dbff762173814dfbbec5213b712d0eff175

Fixed Bug on RemoveAllItemsByDefindex(). - It wasn't removing any items. - note: bug introduced in ce285d96e9a9f9b00b685ad925e0fe7347829757 along with RemoveAllItemsByDefindex().

SteamTrade/Trade.cs
@@ -317,7 +317,7 @@ namespace SteamTrade
 
             foreach (Inventory.Item item in items)
             {
-                if (item != null && !myOfferedItems.ContainsValue (item.Id))
+                if (item != null && myOfferedItems.ContainsValue (item.Id))
                 {
                     bool success = RemoveItem (item.Id);
 