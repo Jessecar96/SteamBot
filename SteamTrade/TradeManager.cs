0cdbc096d66d1d43b05de5c930b62f90a428aa4c
Cainan Whelchel <c.whelchel@gmail.com>
2013-05-18 13:56:20 -0400

    Adds code to fetch foriegn inventory for trade.
    Also removes some private inventory checking and adds an additional check for web errors when trading.
    Fetching is working and now must implement using the foriegn inventory.
    
    Adds ForeignInventory class.
    
    To help with the JSON returns from the trade api call of the same name. Also use it correctly in Trade
    
    Implement the removal of items with foreign inventories.
    
    Testing with a private BP seems to work. Also, adds comments.
    
    Conflicts:
    	SteamTrade/TradeSession.cs
    
    Add ForeignInventory.cs to project.
    
    Adds using for steam kit
    
    Conflicts:
    	SteamTrade/Trade.cs
    
    Start logic for using version number rolloverst to get trade assets. WIP.
    
    Cleanup and comment.
    
    Move TradeSession.cs into a webapi folder. About to refactor this out also.
    
    Refactored out tradestatus api result into its own class.
    
    Also make TradeSession a class and pass in needed state (also extend to use appId instead of hardcoded to 440)
    
    Use new TradeSession for the trade web api interaction.
    
    Also fix validation for new way of getting offered and steamMyOffered items
    
    Cleanup Trade.
    
    Remove apikey from Trade ctor and refactor and rename some methods.
    Add comments to/cleanup TradeSession and Init it on construction.
    
    Remove some debugging statements.

--- SteamTrade/TradeManager.cs
+++ SteamTrade/TradeManager.cs
@@ -176,7 +176,7 @@ namespace SteamTrade
             if (OtherInventory == null || MyInventory == null)
                 InitializeTrade (me, other);
 
-            var t = new Trade (me, other, sessionId, token, apiKey, MyInventory, OtherInventory);
+            var t = new Trade (me, other, sessionId, token, MyInventory, OtherInventory);
 
             t.OnClose += delegate
             {
@@ -227,10 +227,10 @@ namespace SteamTrade
             // fetch other player's inventory from the Steam API.
             OtherInventory = Inventory.FetchInventory (other.ConvertToUInt64 (), apiKey);
 
-            if (OtherInventory == null)
-            {
-                throw new InventoryFetchException (other);
-            }
+            //if (OtherInventory == null)
+            //{
+            //    throw new InventoryFetchException (other);
+            //}
             
             // fetch our inventory from the Steam API.
             MyInventory = Inventory.FetchInventory (me.ConvertToUInt64 (), apiKey);
