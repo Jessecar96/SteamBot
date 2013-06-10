0a22bf8e847c841bec5f57e58b089deb0c5a1376
StormReaper <zenatomiser@gmail.com>
2013-06-10 17:37:03 +0100

    Add item position gc messages

--- SteamBot/TF2GC/Items.cs
+++ SteamBot/TF2GC/Items.cs
@@ -27,5 +27,34 @@ namespace SteamBot.TF2GC
 
             bot.SteamGameCoordinator.Send(deleteMsg, 440);
         }
+
+        /// <summary>
+        /// Sets an item's 1-based position in the backpack
+        /// </summary>
+        /// <param name="bot">The current Bot</param>
+        /// <param name="item">The 64-bit Item ID to move</param>
+        /// <param name="position">The 1-based integer position of the item</param>
+        /// <remarks>
+        /// You must have set the current game to 440 for this to do anything.
+        /// </remarks>
+        public static void SetItemPosition(Bot bot, SteamTrade.Inventory.Item item, short position)
+        {
+            if (bot.CurrentGame != 440)
+                throw new Exception ("SteamBot is not ingame with AppID 440; current AppID is " + bot.CurrentGame);
+
+            byte[] bPos = BitConverter.GetBytes (position);
+            byte[] bClass = BitConverter.GetBytes (-32768);
+
+            byte[] nInv = new byte[] { bPos[0], bPos[1], bClass[0], bClass[1] };
+
+            uint newInventoryDescriptor = BitConverter.ToUInt32 (nInv, 0);
+
+            var aMsg = new ClientGCMsg<MsgSetItemPosition> ();
+
+            aMsg.Write ((long)item.Id);
+            aMsg.Write ((uint)newInventoryDescriptor);
+
+            bot.SteamGameCoordinator.Send (aMsg, 440);
+        }
     }
 }
---
+++ SteamBot/TF2GC/MsgSetItemPosition.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using SteamKit2;
+using SteamKit2.GC;
+using SteamKit2.GC.TF2;
+using SteamKit2.Internal;
+
+namespace SteamBot.TF2GC
+{
+    public class MsgSetItemPosition : IGCSerializableMessage
+    {
+
+        public MsgSetItemPosition()
+        {
+
+        }
+
+        public bool IsProto
+        {
+            get { return false; }
+        }
+
+        public uint MsgType
+        {
+            get { return 1001; }
+        }
+
+        public JobID TargetJobID
+        {
+            get { return new JobID (); }
+            set { throw new NotImplementedException (); }
+        }
+
+        public JobID SourceJobID
+        {
+            get { return new JobID (); }
+            set { throw new NotImplementedException (); }
+        }
+
+        public byte[] Serialize()
+        {
+            List<byte> ret = new List<byte> ();
+            return ret.ToArray ();
+        }
+
+        public void Serialize(Stream stream)
+        {
+            byte[] buf = Serialize ();
+            stream.Write (buf, 0, buf.Length);
+        }
+
+        public void Deserialize(Stream stream)
+        {
+
+        }
+
+        public uint GetEMsg()
+        {
+            //1001 is the EMsg for item position (see SteamKit2's enum somewhere)
+            return 1001;
+        }
+    }
+}
