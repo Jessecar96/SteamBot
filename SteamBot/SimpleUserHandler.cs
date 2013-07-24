b4d21ae8545cfb2a8001cb64568404341f536042
sy1989 <me.sunyue0@gmail.com>
2013-07-24 12:43:20 +0800

    20%

--- SteamBot/SimpleUserHandler.cs
+++ SteamBot/SimpleUserHandler.cs
@@ -12,6 +12,9 @@ namespace SteamBot
        // string[] playerCardName = new string[85] { " Akke ", " Loda ", " AdmiralBulldog ", " EGM ", " S4 ", " universe ", " sneyking ", " Aui_2000 ", " Waytosexy ", " fogged ", " BurNIng ", " Super ", " rOtk ", " QQQ ", " X!! ", " Fly ", " N0tail ", " Era ", " H4nn1 ", " Trixi ", " ChuaN ", " Zhou ", " Ferrari_430 ", " YYF ", " Faith ", " xiao8 ", " DDC ", " Yao ", " Sylar ", " DD ", " Misery ", " Pajkatt ", " God ", " 1437 ", " Brax ", " ixmike88 ", " FLUFFNSTUFF ", " TC ", " Bulba ", " Korok ", " Black^ ", " syndereN ", " FATA ", " paS ", " qojqva ", " Winter ", " FzFz ", " TFG ", " Ling ", " dabeliuteef ", " Dendi ", " XBOCT ", " Puppey ", " Funn1k ", " KuroKy ", " Mushi ", " Xtinct ", " ohayo ", " ky.xy ", " net ", " 7ckngmad ", " Funzii ", " Sockshka ", " Silent ", " Goblak ", " Kabu ", " Lanm ", " Sag ", " Icy ", " Luo ", " Hao ", " Mu ", " Sansheng ", " KingJ ", " Banana ", " ARS-ART ", " NS ", " KSi ", " Crazy ", " Illidan ", " iceiceice ", " xFreedom ", " xy ", " Yamateh ", " Ice " };
         string[] playerCardName = new string[85] { " akke ", " loda ", " admiralbulldog ", " egm ", " s4 ", " universe ", " sneyking ", " aui_2000 ", " waytosexy ", " fogged ", " burning ", " super ", " rotk ", " qqq ", " x!! ", " fly ", " n0tail ", " era ", " h4nn1 ", " trixi ", " chuan ", " zhou ", " ferrari_430 ", " yyf ", " faith ", " xiao8 ", " ddc ", " yao ", " sylar ", " dd ", " misery ", " pajkatt ", " god ", " 1437 ", " brax ", " ixmike88 ", " fluffnstuff ", " tc ", " bulba ", " korok ", " black^ ", " synderen ", " fata ", " pas ", " qojqva ", " winter ", " fzfz ", " tfg ", " ling ", " dabeliuteef ", " dendi ", " xboct ", " puppey ", " funn1k ", " kuroky ", " mushi ", " xtinct ", " ohayo ", " ky.xy ", " net ", " 7ckngmad ", " funzii ", " sockshka ", " silent ", " goblak ", " kabu ", " lanm ", " sag ", " icy ", " luo ", " hao ", " mu ", " sansheng ", " kingj ", " banana ", " ars-art ", " ns ", " ksi ", " crazy ", " illidan ", " iceiceice ", " xfreedom ", " xy ", " yamateh ", " ice " };
         int[] playerCardDefindex = new int[85] { 10217, 10218, 10263, 10264, 10265, 10231, 10272, 10273, 10274, 10275, 10234, 10235, 10236, 10237, 10238, 10266, 10267, 10268, 10269, 10270, 10196, 10207, 10208, 10209, 10210, 10246, 10247, 10248, 10249, 10250, 10239, 10240, 10241, 10242, 10282, 10205, 10219, 10220, 10221, 10271, 10244, 10245, 10288, 10289, 10290, 10243, 10283, 10284, 10285, 10286, 10197, 10222, 10223, 10224, 10225, 10215, 10216, 10260, 10261, 10262, 10252, 10253, 10254, 10255, 10256, 10257, 10258, 10291, 10292, 10293, 10211, 10212, 10213, 10214, 10259, 10233, 10276, 10277, 10278, 10279, 10226, 10227, 10228, 10229, 10251 };
+        int[] playerCardInventory=new int[85];
+        bool[] playerCardOk = new bool[85];
+        int totalPlayCardInventory = 0;
         static int TimerInterval = 170000;
         static int InviteTimerInterval = 2000;
         public SimpleUserHandler (Bot bot, SteamID sid) : base(bot, sid) 
@@ -82,9 +85,43 @@ namespace SteamBot
         public override void OnTradeInit() 
         {
             ReInit();
+            TradeCountInventory(true);
             Trade.SendMessage ("Success. Please put up your items.And type add xxx yyy or remove xxx yyy to ask bot to add or remove player card");
         }
-        
+
+        public bool  TradeCountInventory(bool ok)
+        {
+            totalPlayCardInventory = 0;
+            for (int ii = 0; ii < 85; ii++)
+            {
+
+                playerCardInventory[ii] = 0;
+
+            }
+            // Let's count our inventory
+            Inventory.Item[] inventory = Trade.MyInventory.Items;
+            
+            foreach (Inventory.Item item in inventory)
+            {
+               
+                for (int ii = 0; ii < 85; ii++)
+                {
+                    if (item.Defindex == playerCardDefindex[ii])
+                    {
+                        playerCardInventory[ii]++;
+                    }
+                }
+            }
+            for (int ii = 0; ii < 85; ii++)
+            {
+
+                totalPlayCardInventory = totalPlayCardInventory + playerCardInventory[ii] ;
+
+            }
+            return true;
+            
+        }
+
         public override void OnTradeAddItem (Schema.Item schemaItem, Inventory.Item inventoryItem) 
         {
             var item = Trade.CurrentSchema.GetItem(schemaItem.Defindex);//获取添加物品信息并赋予变量item
@@ -98,13 +135,22 @@ namespace SteamBot
                     {
                         userCardAdded = userCardAdded + 3;
                     }
-                    else if (i < 5 || i == 52 || i == 54 || (19 < i && i < 25) || (15 < i && i < 19))
+                    else if (i < 5 || i == 52 || i == 54 || (19 < i && i < 25))
                     {
                         userCardAdded = userCardAdded + 2;
                     }
                     else
                     {
-                        userCardAdded++;
+
+                        if (playerCardInventory[i] * 5 > totalPlayCardInventory)
+                        {
+                            Trade.SendMessage("因为这种卡片已超过卡片总数的20%,我现在不接受这种卡片 ");
+                        }
+                        else
+                        {
+
+                            userCardAdded++;
+                        }
                     }
                 }
                 else
@@ -136,7 +182,14 @@ namespace SteamBot
                     }
                     else
                     {
-                        userCardAdded--;
+                        if (playerCardInventory[i] * 5 > totalPlayCardInventory)
+                        {
+                            Trade.SendMessage("你移除了我不接受的卡片 ");
+                        }
+                        else
+                        {
+                            userCardAdded--;
+                        }
                     }
                 }
                 else
