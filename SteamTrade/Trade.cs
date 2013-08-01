c4fee3eb448f4473c2e2fa4735fd7e18df735db6
FunkyLoveCow <mosquito@mailinator.com>
2013-08-01 08:01:25 -0500

    Code style changes

--- SteamTrade/Trade.cs
+++ SteamTrade/Trade.cs
@@ -582,13 +582,13 @@ namespace SteamTrade
         {
             ulong itemID = tradeEvent.assetid;
 
-            if (null != OtherInventory)
+            if (OtherInventory != null)
             {
                 Inventory.Item item = OtherInventory.GetItem(itemID);
-                if (null != item)
+                if (item != null)
                 {
                     Schema.Item schemaItem = CurrentSchema.GetItem(item.Defindex);
-                    if (null == schemaItem)
+                    if (schemaItem == null)
                     {
                         // TODO: Log this (counldn't find the item in CurrentSchema
                     }
@@ -604,7 +604,7 @@ namespace SteamTrade
             else
             {
                 var schemaItem = GetItemFromPrivateBp(tradeEvent, itemID);
-                if (null == schemaItem)
+                if (schemaItem == null)
                 {
                     // TODO: Log this (couldn't find item in CurrentSchema)
                 }
@@ -642,10 +642,10 @@ namespace SteamTrade
             if (OtherInventory != null)
             {
                 Inventory.Item item = OtherInventory.GetItem(itemID);
-                if (null != item)
+                if (item != null)
                 {
                     Schema.Item schemaItem = CurrentSchema.GetItem(item.Defindex);
-                    if (null == schemaItem)
+                    if (schemaItem == null)
                     {
                         // TODO: Add log (counldn't find item in CurrentSchema)
                     }
@@ -661,7 +661,7 @@ namespace SteamTrade
             else
             {
                 var schemaItem = GetItemFromPrivateBp(tradeEvent, itemID);
-                if (null == schemaItem)
+                if (schemaItem == null)
                 {
                     // TODO: Add log (counldn't find item in CurrentSchema)
                 }
