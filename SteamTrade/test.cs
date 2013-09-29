32f419777dabfbd80bdbc5a035505fa6b92a98f6
sy1989 <me.sunyue0@gmail.com>
2013-09-29 17:53:25 +0800

    aaa
    aaa

--- SteamTrade/test.cs
+++ SteamTrade/test.cs
@@ -20,7 +20,7 @@ namespace SteamTrade
     {
         private const string SchemaMutexName = "dota2items.json";
         private const string cachefile = "dota2items.json";
-
+        private const string cachefileinfo = "dota2info.txt";
         /// <summary>
         /// Fetches the Tf2 Item schema.
         /// </summary>
@@ -39,32 +39,122 @@ namespace SteamTrade
             // write the cache file. The others will wait patiently.
             if (!wasCreated)
             {
-                bool signaled = mre.WaitOne(10000);
+                bool signaled = false;
 
-                if (!signaled)
+                while (!signaled)
                 {
-                    return null;
+                    signaled = mre.WaitOne(10000);
                 }   
             }
-            HttpWebRequest request = (HttpWebRequest)HttpWebRequest.Create(url);
-            request.Method = "GET";
-            request.Accept = "text/javascript, text/html, application/xml, text/xml, */*";
-            request.ContentType = "application/x-www-form-urlencoded; charset=UTF-8";
-            request.Host = "media.steampowered.com";
-            request.UserAgent = "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/536.11 (KHTML, like Gecko) Chrome/20.0.1132.47 Safari/536.11";
-            request.Referer = "http://media.steampowered.com";
-            HttpWebResponse response = request.GetResponse() as HttpWebResponse;
-            //DateTime schemaLastModified = DateTime.Parse(response.Headers["Last-Modified"]);
-            Stream urlresult = response.GetResponseStream();
-            //string result = GetSchemaString();
-           // TextWriter aaa = new StreamWriter(cachefile ,true);
-            //aaa.Write(urlresult);
-            Convertvdf2json(urlresult, cachefile, false);
-            response.Close();
-            request.Abort();
-            string result = GetSchemaString();
-            SchemaResult schemaResult = JsonConvert.DeserializeObject<SchemaResult>(result);
-            return schemaResult.items_game ?? null;
+            bool needhttp = true;
+            SchemaResult schemaResult=null;
+            if (File.Exists (cachefile)&&File.Exists (cachefileinfo ))
+            {
+                Console.Write("info及json存在");
+                string cache;
+                TextReader readercache = new StreamReader(cachefileinfo );
+                cache = readercache.ReadToEnd();
+                readercache.Close();
+                if (cache == url)
+                {
+                    Console.Write("url未变化");
+                //    try
+                //    {
+
+                        HttpWebRequest request = (HttpWebRequest)HttpWebRequest.Create(url);
+                        request.Method = "HEAD";
+                        request.Accept = "text/javascript, text/html, application/xml, text/xml, */*";
+                        request.ContentType = "application/x-www-form-urlencoded; charset=UTF-8";
+                        request.Host = "media.steampowered.com";
+                        request.UserAgent = "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/536.11 (KHTML, like Gecko) Chrome/20.0.1132.47 Safari/536.11";
+                        request.Referer = "http://media.steampowered.com";
+                        request.IfModifiedSince = File.GetCreationTime(cachefile);
+                        HttpWebResponse response = request.GetResponse() as HttpWebResponse;
+                        if (response.StatusCode == HttpStatusCode.NotModified)
+                        {
+                            needhttp = false;
+                            Console.Write("文件未修改,无需下载");
+                        }
+                        else
+                        {
+                            needhttp = true;
+                            Console.Write("文件已修改,重新下载");
+                        }
+                 //   }
+                   /* catch
+                    {
+                        Console.WriteLine("http状态获取失败");
+                        needhttp = true;
+                    } */
+                }
+                else
+                {
+                    Console.WriteLine(cache.Length);
+                    Console.WriteLine(url.Length);
+                    Console.WriteLine();
+                    Console.Write(cache);
+                    Console.WriteLine();
+                    Console.Write(url);
+                    Console.WriteLine();
+                    Console.Write("url已变化");
+                    Console.WriteLine();
+                }
+            }
+            if (!needhttp)
+            {
+                Console.Write("需要重新下载");
+                try
+                {
+                    StreamReader reader = new StreamReader(cachefile);
+                    Stream urlresult = reader.BaseStream ;
+                    //string result = GetSchemaString();
+                    // TextWriter aaa = new StreamWriter(cachefile ,true);
+                    //aaa.Write(urlresult);
+                    Convertvdf2json(urlresult, cachefile, false);
+                    string result = GetSchemaString();
+                    schemaResult = JsonConvert.DeserializeObject<SchemaResult>(result);
+                    //return schemaResult.items_game ?? null;
+                }
+                catch
+                {
+                    needhttp = true;
+                }
+            }
+            while (needhttp) //假如已保存的文件无效则连接http获取新的文件.
+            {
+                try
+                {
+                    needhttp = false;
+                    HttpWebRequest request = (HttpWebRequest)HttpWebRequest.Create(url);
+                    request.Method = "GET";
+                    request.Accept = "text/javascript, text/html, application/xml, text/xml, */*";
+                    request.ContentType = "application/x-www-form-urlencoded; charset=UTF-8";
+                    request.Host = "media.steampowered.com";
+                    request.UserAgent = "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/536.11 (KHTML, like Gecko) Chrome/20.0.1132.47 Safari/536.11";
+                    request.Referer = "http://media.steampowered.com";
+                    HttpWebResponse response = request.GetResponse() as HttpWebResponse;
+                    DateTime schemaLastModified = DateTime.Parse(response.Headers["Last-Modified"]);
+                    Stream urlresult = response.GetResponseStream();
+                    Convertvdf2json(urlresult, cachefile, false);
+                    File.SetCreationTime(cachefile, schemaLastModified);
+                    StreamWriter wf = new StreamWriter(cachefileinfo , false);
+                    wf.Write(url);
+                    wf.Close();
+                    response.Close();
+                    request.Abort();
+                    string result = GetSchemaString();
+                    schemaResult = JsonConvert.DeserializeObject<SchemaResult>(result);
+                   // return schemaResult.items_game ?? null;
+
+                }
+                catch
+                {
+                    needhttp = true;
+                }
+ 
+            }
+            return schemaResult.items_game ?? null; 
+            
         }
         public static void Convertvdf2json(Stream inputstream, string outputpath, bool CompactJSON)
         {
