2bdff7d0bdef46ed4716c050b57d3cb753a468a9
Anthony Garcia <anthony@lagg.me>
2013-11-07 23:54:11 -0700

    Remove commands from ci scripts that could give nasty surprises later

--- .ci/build.sh
+++ .ci/build.sh
@@ -6,14 +6,5 @@ function ExitIfNonZero {
 	fi
 }
 
-wget -P .nuget https://nuget.org/nuget.exe 
-ExitIfNonZero $?
-
-mv .nuget/nuget.exe .nuget/NuGet.exe
-ExitIfNonZero $?
-
-cp /usr/lib/mono/4.0/Microsoft.Build.dll .nuget/Microsoft.Build.dll
-ExitIfNonZero $?
-
 xbuild /p:NoWarn=1584 /property:Configuration=Debug /property:Platform="Any CPU" SteamBot.sln 
 ExitIfNonZero $?
