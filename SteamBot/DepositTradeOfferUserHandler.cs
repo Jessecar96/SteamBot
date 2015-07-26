5810e4438fa7b727fc5ef2f6290c2f31b433fb5a
Jordan Turley <ztizzlegaming@gmail.com>
2015-07-26 16:14:02 -0400

    Create Deposit and Profit Trade Offer Handlers

--- SteamBot/DepositTradeOfferUserHandler.cs
+++ SteamBot/DepositTradeOfferUserHandler.cs
@@ -16,12 +16,15 @@ using Newtonsoft.Json.Linq;
 
 namespace SteamBot
 {
-    public class TradeOfferUserHandler : UserHandler
+    public class DepositTradeOfferUserHandler : UserHandler
     {
-        public TradeOfferUserHandler(Bot bot, SteamID sid) : base(bot, sid) { }
+        public DepositTradeOfferUserHandler(Bot bot, SteamID sid) : base(bot, sid) { }
 
         public override void OnNewTradeOffer(TradeOffer offer)
         {
+			var botInfo = new Configuration.BotInfo();
+			string pass = botInfo.Password;
+
 			Log.Info ("Trade offer recieved from user ID: " + offer.PartnerSteamId);
 
 			//Get items in the trade, and ID of user sending trade
@@ -96,92 +99,16 @@ namespace SteamBot
 				var response = (HttpWebResponse)request.GetResponse();
 
 				var responseString = new StreamReader(response.GetResponseStream()).ReadToEnd();
+
+				//Check responseString to see if it is the end of a round
+				//If it is, send out the trade to the winner, and send offer to the profit account, with ~2% of items
 			}
-
-
-
-
-
-			//Example code given:
-            //receiving a trade offer 
-            /* if (IsAdmin)
-            {
-                //parse inventories of bot and other partner
-                //either with webapi or generic inventory
-                //Bot.GetInventory();
-                //Bot.GetOtherInventory(OtherSID);
-
-                var myItems = offer.Items.GetMyItems();
-                var theirItems = offer.Items.GetTheirItems();
-                Log.Info("They want " + myItems.Count + " of my items.");
-                Log.Info("And I will get " +  theirItems.Count + " of their items.");
-
-                //do validation logic etc
-                if (DummyValidation(myItems, theirItems))
-                {
-                    string tradeid;
-                    if (offer.Accept(out tradeid))
-                    {
-                        Log.Success("Accepted trade offer successfully : Trade ID: " + tradeid);
-                    }
-                }
-                else
-                {
-                    // maybe we want different items or something
-
-                    //offer.Items.AddMyItem(0, 0, 0);
-                    //offer.Items.RemoveTheirItem(0, 0, 0);
-                    if (offer.Items.NewVersion)
-                    {
-                        string newOfferId;
-                        if (offer.CounterOffer(out newOfferId))
-                        {
-                            Log.Success("Counter offered successfully : New Offer ID: " + newOfferId);
-                        }
-                    }
-                }
-            }
-            else
-            {
-                //we don't know this user so we can decline
-                if (offer.Decline())
-                {
-                    Log.Info("Declined trade offer : " + offer.TradeOfferId + " from untrusted user " + OtherSID.ConvertToUInt64());
-                }
-            } */
         }
 
         public override void OnMessage(string message, EChatEntryType type)
         {
-            if (IsAdmin)
-            {
-                //creating a new trade offer
-                var offer = Bot.NewTradeOffer(OtherSID);
+			SendChatMessage (Bot.ChatResponse);
 
-                //offer.Items.AddMyItem(0, 0, 0);
-                if (offer.Items.NewVersion)
-                {
-                    string newOfferId;
-                    if (offer.Send(out newOfferId))
-                    {
-                        Log.Success("Trade offer sent : Offer ID " + newOfferId);
-                    }
-                }
-
-                //creating a new trade offer with token
-                var offerWithToken = Bot.NewTradeOffer(OtherSID);
-
-                //offer.Items.AddMyItem(0, 0, 0);
-                if (offerWithToken.Items.NewVersion)
-                {
-                    string newOfferId;
-                    // "token" should be replaced with the actual token from the other user
-                    if (offerWithToken.SendWithToken(out newOfferId, "token"))
-                    {
-                        Log.Success("Trade offer sent : Offer ID " + newOfferId);
-                    }
-                }
-            }
         }
 
         public override bool OnGroupAdd() { return false; }
@@ -211,16 +138,6 @@ namespace SteamBot
         public override void OnTradeReady(bool ready) { }
 
         public override void OnTradeAccept() { }
-
-        private bool DummyValidation(List<TradeAsset> myAssets, List<TradeAsset> theirAssets)
-        {
-            //compare items etc
-            if (myAssets.Count == theirAssets.Count)
-            {
-                return true;
-            }
-            return false;
-        }
     }
 
 	public class CSGOItem {
