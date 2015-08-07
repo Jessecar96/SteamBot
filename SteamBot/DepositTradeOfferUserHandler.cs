ebddbab945a8af7939e5ef695ea9106b92af3e9d
Jordan Turley <ztizzlegaming@gmail.com>
2015-08-06 22:30:36 -0400

    Payouts pretty much working
    The only part that isn't working is if there are multiple of the same
    item.

--- SteamBot/DepositTradeOfferUserHandler.cs
+++ SteamBot/DepositTradeOfferUserHandler.cs
@@ -64,8 +64,6 @@ namespace SteamBot
 
 			postData += "&items=" + theirItemsJSON;
 
-			Log.Success ("Post data string: \n" + postData);
-
 			string url = "http://csgowinbig.jordanturley.com/php/deposit.php";
 			var request = (HttpWebRequest)WebRequest.Create (url);
 
@@ -83,7 +81,7 @@ namespace SteamBot
 
 			var responseString = new StreamReader(response.GetResponseStream()).ReadToEnd();
 
-			Log.Success ("Response received from server: \n" + responseString);
+			//Log.Success ("Response received from server: \n" + responseString);
 
 			JSONClass responseJsonObj = JsonConvert.DeserializeObject<JSONClass> (responseString);
 
@@ -98,21 +96,90 @@ namespace SteamBot
 
 					//Check if the pot is over
 					if (jsonData.potOver == 1) {
-						//Get the winner, and items to give and keep
-
+						//Get items to give and keep, and the winner and their trade token
 						var itemsToGive = jsonData.tradeItems;
 						var itemsToKeep = jsonData.profitItems;
 
 						string winnerSteamIDString = jsonData.winnerSteamID;
 						SteamID winnerSteamID = new SteamID (winnerSteamIDString);
 
+						string winnerTradeToken = jsonData.winnerTradeToken;
+
+						Log.Success ("Winner steam id: " + winnerSteamIDString + ", token: " + winnerTradeToken);
+
+						//Get bot's inventory json
+						string botInvUrl = "http://steamcommunity.com/profiles/76561198238743988/inventory/json/730/2";
+						var botInvRequest = (HttpWebRequest)WebRequest.Create (botInvUrl);
+						var botInvResponse = (HttpWebResponse)botInvRequest.GetResponse ();
+						string botInvString = new StreamReader (botInvResponse.GetResponseStream()).ReadToEnd();
+
+						BotInventory botInventory = JsonConvert.DeserializeObject<BotInventory> (botInvString);
+						if (botInventory.success != true) {
+							Log.Error ("An error occured while fetching the bot's inventory.");
+							return;
+						}
+						var rgInventory = botInventory.rgInventory;
+
+
 						//Create trade offer for the winner
-						var tradeOffer = Bot.NewTradeOffer (winnerSteamID);
+						var winnerTradeOffer = Bot.NewTradeOffer (winnerSteamID);
 
 						//Loop through all winner's items and add them to trade
-						foreach (CSGOItem item in itemsToGive) {
-							
+						foreach (CSGOItemFromWeb item in itemsToGive) {
+							long classId = item.classId, instanceId = item.instanceId;
+
+							//Loop through all inventory items and find the asset id for the item
+							long assetId = 0;
+							foreach (var inventoryItem in rgInventory) {
+								var value = inventoryItem.Value;
+								long tAssetId = value.id, tClassId = value.classid, tInstanceId = value.instanceid;
+
+								if (tClassId == classId && tInstanceId == instanceId) {
+									assetId = tAssetId;
+								}
+							}
+
+							//Log.Success ("Adding item to winner trade offer. Asset ID: " + assetId);
+
+							winnerTradeOffer.Items.AddMyItem (730, 2, assetId, 1);
 						}
+
+						//Send trade offer to winner
+						string winnerTradeOfferId, winnerMessage = "Congratulations, you have won on CSGO Win Big! Here are your items.";
+						winnerTradeOffer.SendWithToken (out winnerTradeOfferId, winnerTradeToken, winnerMessage);
+						Log.Success ("Offer sent to winner.");
+
+						//System.Threading.Thread.Sleep (1000);
+
+						//Create trade offer for items kept
+						string profitSteamIDString = "STEAM_0:1:66955921";
+						SteamID profitSteamID = new SteamID (profitSteamIDString);
+						var profitTradeOffer = Bot.NewTradeOffer (profitSteamID);
+
+						//Loop through all profit items and add them to trade
+						foreach (CSGOItemFromWeb item in itemsToKeep) {
+							long classId = item.classId, instanceId = item.instanceId;
+
+							//Loop through all inventory items and find the asset id for the item
+							long assetId = 0;
+							foreach (var inventoryItem in rgInventory) {
+								var value = inventoryItem.Value;
+								long tAssetId = value.id, tClassId = value.classid, tInstanceId = value.instanceid;
+
+								if (tClassId == classId && tInstanceId == instanceId) {
+									assetId = tAssetId;
+								}
+							}
+
+							//Log.Success ("Adding item to winner trade offer. Asset ID: " + assetId);
+
+							profitTradeOffer.Items.AddMyItem (730, 2, assetId, 1);
+						}
+
+						//Send trade offer to profit account
+						string profitTradeOfferId;
+						profitTradeOffer.Send (out profitTradeOfferId, "Profit items.");
+						Log.Success ("Offer sent to profit.");
 					}
 				} else {
 					if (offer.Decline ()) {
@@ -128,7 +195,6 @@ namespace SteamBot
         public override void OnMessage(string message, EChatEntryType type)
         {
 			SendChatMessage (Bot.ChatResponse);
-
         }
 
         public override bool OnGroupAdd() { return false; }
@@ -176,14 +242,25 @@ namespace SteamBot
 		public int minDeposit;
 		public int potOver;
 		public string winnerSteamID;
-		public List<CSGOItem> tradeItems;
-		public List<CSGOItem> profitItems;
+		public string winnerTradeToken;
+		public List<CSGOItemFromWeb> tradeItems;
+		public List<CSGOItemFromWeb> profitItems;
 	}
 
 	public class CSGOItemFromWeb {
-		public long appId;
-		public long contextId;
-		public long assetId;
+		public long classId;
+		public long instanceId;
+	}
 
+	//Classes for json bot inventory
+	public class BotInventory {
+		public bool success;
+		public IDictionary<string, inventoryItem> rgInventory;
+	}
+
+	public class inventoryItem {
+		public long id;
+		public long classid;
+		public long instanceid;
 	}
 }
