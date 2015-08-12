f279cf7b012c74a8f41acf33ad0d67242e6aa67a
Jordan Turley <ztizzlegaming@gmail.com>
2015-08-11 21:54:02 -0400

    Check for more than 10 items

--- SteamBot/DepositTradeOfferUserHandler.cs
+++ SteamBot/DepositTradeOfferUserHandler.cs
@@ -46,6 +46,12 @@ namespace SteamBot
 				}
 			}
 
+			//Check if there are more than 10 items in the trade
+			if (theirItems.Count > 10) {
+				shouldDecline = true;
+				Log.Error ("Offer declined because there were more than 10 items in the deposit.");
+			}
+
 			if (shouldDecline) {
 				if (offer.Decline ()) {
 					Log.Error ("Offer declined.");
