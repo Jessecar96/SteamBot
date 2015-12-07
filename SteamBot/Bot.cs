c272a18b0eda83129ac3d64222322870c9361167
Peter Willemsen <peterw.email@gmail.com>
2015-12-07 23:06:02 +0100

    Only delay if not entered mobile details yet

--- SteamBot/Bot.cs
+++ SteamBot/Bot.cs
@@ -713,14 +713,15 @@ namespace SteamBot
 
             msg.Handle<SteamClient.DisconnectedCallback> (callback =>
             {
+                var mobileAuthCode = GetMobileAuthCode();
                 if(IsLoggedIn)
                 {
                     IsLoggedIn = false;
                     CloseTrade();
                     Log.Warn("Disconnected from Steam Network!");
-                }
-                
-                Thread.Sleep(60 * 1000);
+				} else if(string.IsNullOrEmpty(mobileAuthCode)) {						
+					Thread.Sleep(60 * 1000);
+				}
                 SteamClient.Connect ();
             });
             #endregion
