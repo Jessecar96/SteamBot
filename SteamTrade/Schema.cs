c37be543d23dcf55f047b30a78c06ca338547c09
fjch1 <fjch1@LLOYD-PC-XPS15>
2016-06-29 09:40:32 +0800

    Removed C# 6

--- SteamTrade/Schema.cs
+++ SteamTrade/Schema.cs
@@ -16,10 +16,16 @@ namespace SteamTrade
     {
         private const string SchemaMutexName = "steam_bot_cache_file_mutex";
         private const string SchemaApiUrlBase = "http://api.steampowered.com/IEconItems_440/GetSchema/v0001/?key=";
+        
         /// <summary>
         /// Full file name for schema cache file. This value is only used when calling <see cref="FetchSchema"/>. If the time modified of the local copy is later than that of the server, local copy is used without downloading. Default value is %TEMP%\tf_schema.cache.
         /// </summary>
-        public static string CacheFileFullName { get; set; } = Path.GetTempPath() + "\\tf_schema.cache";
+        public static string CacheFileFullName
+        {
+            get { return cacheFileFullName; }
+            set { cacheFileFullName = value; }
+        }
+        private static string cacheFileFullName = Path.GetTempPath() + "\\tf_schema.cache";
 
         /// <summary>
         /// Fetches the Tf2 Item schema.
