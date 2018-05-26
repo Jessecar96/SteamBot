7b720a8d330db3be364c2c00f046a657b2cd1455
kilpkonn <tavo.annus@gmail.com>
2018-05-26 09:47:36 +0300

    Forgot that

--- SteamTrade/Schema.cs
+++ SteamTrade/Schema.cs
@@ -122,6 +122,9 @@ namespace SteamTrade
 
         [JsonProperty("originNames")]
         public ItemOrigin[] OriginNames { get; set; }
+        
+        [JsonProperty("next")]
+        public int Next { get; set; }
 
         /// <summary>
         /// Find an SchemaItem by it's defindex.
